0aaa1580fd4e1ec68538821e315bdf1fac98107b
Saplonily <Saplonily@outlook.com>
2023-05-13 21:59:06 +0800

    small fix

--- Board/UI/WaveTitle/WaveTitle.cs
+++ Board/UI/WaveTitle/WaveTitle.cs
@@ -25,6 +25,6 @@ public partial class WaveTitle : Node2D
         animationPlayer.Play("Appear");
         animationPlayer.Advance(0d);
         await ToSignal(animationPlayer, AnimationPlayer.SignalName.AnimationFinished);
-        Free();
+        QueueFree();
     }
 }
--- Init.cs
+++ Init.cs
@@ -20,12 +20,13 @@ public partial class Init : Node
         //};
         //sd.SaveToUser("user://temp_test.bin");
         Engine.TimeScale = TimeScale;
+        Game.Logger.LogInfo("S", "What the fuck?");
         Game.Instance.MainScene = MainScene;
 #if GODOT_WINDOWS
         HRESULT hr = Native.ComInit();
         if (hr is not >= 0)
         {
-            _ = Native.NativeMessageBox("警告", "COM初始化失败");
+            _ = Native.NativeMessageBox("错误", "COM初始化失败");
         }
 #endif
         if (string.IsNullOrEmpty(initGotoLevel))
